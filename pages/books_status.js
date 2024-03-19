20ff196b008306adf5e7d204b926981c0cee73d8
wanjing27 <wanjing0227@gmail.com>
2024-03-18 22:21:20 -0400

    fix error

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,17 @@
-let BookInstance = require('../models/bookinstance');
+const BookInstance = require('../models/bookinstance');
 
 exports.show_all_books_status = function(res) {
-  return res.send([]);
-}   
+  BookInstance.find({}, 'book status')
+    .populate('book', 'title')
+    .exec((err, bookInstances) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Error fetching book instances');
+      }
+      const formattedBookInstances = bookInstances.map(instance => ({
+        title: instance.book.title,
+        status: instance.status
+      }));
+      res.send(formattedBookInstances);
+    });
+}
